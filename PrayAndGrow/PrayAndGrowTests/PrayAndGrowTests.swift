345d1d43180cc0d77c9fa0724a991694d5d79e8c
badeleux <badeleux@gmail.com>
2016-04-09 13:06:42 +0200

    Initial commit

---
+++ PrayAndGrow/PrayAndGrowTests/PrayAndGrowTests.swift
@@ -0,0 +1,36 @@
+//
+//  PrayAndGrowTests.swift
+//  PrayAndGrowTests
+//
+//  Created by Kamil Badyla on 09/04/16.
+//  Copyright © 2016 Hackatheon. All rights reserved.
+//
+
+import XCTest
+@testable import PrayAndGrow
+
+class PrayAndGrowTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
